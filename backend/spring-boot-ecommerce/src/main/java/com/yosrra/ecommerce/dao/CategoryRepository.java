7f2fcdcda235526a8eb6cc149a6e24bf01a08c94
yosraf <yosrafatnassi6@gmail.com>
2020-07-31 14:13:23 +0100

    aded cors

--- backend/spring-boot-ecommerce/src/main/java/com/yosrra/ecommerce/dao/CategoryRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/yosrra/ecommerce/dao/CategoryRepository.java
@@ -2,8 +2,10 @@ package com.yosrra.ecommerce.dao;
 
 import com.yosrra.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-@Repository
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface CategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- backend/spring-boot-ecommerce/src/main/java/com/yosrra/ecommerce/dao/ProductRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/yosrra/ecommerce/dao/ProductRepository.java
@@ -3,7 +3,8 @@ package com.yosrra.ecommerce.dao;
 import com.yosrra.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-@Repository
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
