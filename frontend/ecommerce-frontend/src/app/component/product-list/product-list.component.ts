41edc71d57be90813e78f1d0024407eaf821672d
yosraf <yosrafatnassi6@gmail.com>
2020-07-31 13:52:37 +0100

    implementing product-service

--- frontend/ecommerce-frontend/src/app/component/product-list/product-list.component.ts
+++ frontend/ecommerce-frontend/src/app/component/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from '../../services/product.service';
+import {Product} from '../../common/product';
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  products: Product[];
+
+  constructor(private productListService: ProductService) {
+  }
+
+  listProducts() {
+    this.productListService.getProductList().subscribe(data => {
+      this.products = data;
+    });
+
+  }
 
   ngOnInit(): void {
+    this.listProducts();
   }
 
 }
