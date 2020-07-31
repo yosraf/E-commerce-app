ff3f1e14d4870e1ed9bd8dd3fea8fa22364f9bf9
yosraf <yosrafatnassi6@gmail.com>
2020-07-31 13:29:09 +0100

    created product-list component created product class created product-service added provider and import in appmodule

--- frontend/ecommerce-frontend/src/app/app.module.ts
+++ frontend/ecommerce-frontend/src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ProductListComponent} from './component/product-list/product-list.component';
+import {HttpClientModule} from '@angular/common/http';
+import {ProductService} from './services/product.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
