87f9a275c95a9b361772b3dc25c01f66fee4319d
yosraf <yosrafatnassi6@gmail.com>
2020-08-01 15:46:25 +0100

    adding service and controller packages

--- backend/spring-boot-ecommerce/src/main/java/com/yosrra/ecommerce/dao/CategoryRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/yosrra/ecommerce/dao/CategoryRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
-@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface CategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
