a954ee39b9398c6636332ca4020a3422da269c03
yosraf <yosrafatnassi6@gmail.com>
2020-12-22 20:55:28 +0100

    added the detail product layout

--- frontend/ecommerce-frontend/src/app/app.module.ts
+++ frontend/ecommerce-frontend/src/app/app.module.ts
@@ -1,24 +1,26 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 
-import {AppComponent} from './app.component';
-import {ProductListComponent} from './component/product-list/product-list.component';
-import {HttpClientModule} from '@angular/common/http';
-import {ProductService} from './services/product.service';
-import {RouterModule, Routes} from '@angular/router';
-import {ProductCategoryMenuComponent} from './component/product-category-menu/product-category-menu.component';
-import {SearchComponent} from './component/search/search.component';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './component/product-list/product-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductService } from './services/product.service';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductCategoryMenuComponent } from './component/product-category-menu/product-category-menu.component';
+import { SearchComponent } from './component/search/search.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductDetailComponent } from './component/product-detail/product-detail.component';
 
 const routes: Routes = [
-  {path: 'category/:id', component: ProductListComponent},
-  {path: 'category', component: ProductListComponent},
-  {path: 'products', component: ProductListComponent},
-  {path: 'search/:keyword', component: ProductListComponent},
-  {path: '', redirectTo: '/products', pathMatch: 'full'},
-  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+  { path: 'category/:id', component: ProductListComponent },
+  { path: 'category', component: ProductListComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: 'search/:keyword', component: ProductListComponent },
+  { path: 'detail', component: ProductDetailComponent },
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: '**', redirectTo: '/products', pathMatch: 'full' }
 
 ];
 
@@ -27,7 +29,8 @@ const routes: Routes = [
     AppComponent,
     ProductListComponent,
     ProductCategoryMenuComponent,
-    SearchComponent  ],
+    SearchComponent,
+    ProductDetailComponent],
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
